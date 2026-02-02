f35313d1fed95b7fe30d7e7dfaf18aae25c84a42
programmerwannabe <165480412+juleyjuls0208@users.noreply.github.com>
2026-02-02 21:47:46 +0800

    Delete mobile directory

--- mobile/android/app/src/main/java/com/juls/bankongsetonandroid/Transaction.kt
+++
@@ -1,9 +0,0 @@
-package com.juls.bankongsetonandroid
-
-data class Transaction(
-    val timestamp: String,
-    val type: String,
-    val amount: Double,
-    val balance: Double,
-    val description: String
-)
